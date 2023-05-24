afb2238e738778ad79932d70c73e91e0baf1d319
saorregog <saorregog@gmail.com>
2023-05-24 18:35:21 -0500

    Nueva version y version modificada del algoritmo de calculo de tokens conformados por solo digitos

--- 7. Ejercicio 1 Avanzatech/Ejercicio1.js
+++ 7. Ejercicio 1 Avanzatech/Ejercicio1.js
@@ -1,13 +1,32 @@
 // DISEÑA UNA FUNCIÓN EN JS QUE RECIBE UNA CADENA DE TEXTO Y RETORNA COMO RESULTADO CUÁNTOS TOKENS HAY CONFORMADOS POR SÓLO DÍGITOS. UN TOKEN ES UNA SECUENCIA DE CARACTERES SIN ESPACIOS.
 
-function tokensInString(string) {
+// FUNCIÓN SIN USO DE TRIM()
+function tokensInString1(string) {
   console.log(
     string.split(" ").filter(function (element) {
-      return element == "0" || Boolean(Number(element));
+      return (
+        (element != "" && Number(element) == "0") || Boolean(Number(element))
+      );
     }).length
   );
 }
 
-tokensInString(
-  "Una 234 c4d3na d3 3 palabras de sólo 0 dígitos distribuidos aleatoriamente"
+tokensInString1(
+  " Una 234 c4d3na d3 3 palabras de sólo 000 dígitos distribuidos aleatoriamente "
+);
+
+// FUNCIÓN CON USO DE TRIM
+function tokensInString2(string) {
+  console.log(
+    string
+      .trim()
+      .split(" ")
+      .filter(function (element) {
+        return Number(element) == "0" || Boolean(Number(element));
+      }).length
+  );
+}
+
+tokensInString2(
+  " Una 234 c4d3na d3 3 palabras de sólo 000 dígitos distribuidos aleatoriamente "
 );
