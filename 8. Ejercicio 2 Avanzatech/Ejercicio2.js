444f36b2fa636aa6145c262faa1655b35dd26038
saorregog <saorregog@gmail.com>
2023-05-26 10:17:48 -0500

    Version 2 del algoritmo de suma de los primeros n numeros pares

--- 8. Ejercicio 2 Avanzatech/Ejercicio2.js
+++ 8. Ejercicio 2 Avanzatech/Ejercicio2.js
@@ -1,6 +1,7 @@
 // DISEÑA UNA FUNCIÓN EN JS QUE RECIBA COMO PARÁMETRO UN ENTERO POSITIVO "n" Y RETORNE LA SUMA DE LOS PRIMEROS "n" NÚMEROS PARES.
 
-function sumEvenNumbers(n) {
+// VERSIÓN 1
+function sumEvenNumbers1(n) {
   let sum = 0;
 
   for (let i = 2; i <= n * 2; i += 2) {
@@ -10,4 +11,17 @@ function sumEvenNumbers(n) {
   console.log(sum);
 }
 
-sumEvenNumbers(5);
+sumEvenNumbers1(5);
+
+// VERSIÓN 2
+function sumEvenNumbers2(n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum += 2 * i;
+  }
+
+  console.log(sum);
+}
+
+sumEvenNumbers2(5);
